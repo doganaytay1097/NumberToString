879d6822b81c30146fafa183f30b2b3588ebb8fc
Doğanay Tay <116439695+doganaytay1097@users.noreply.github.com>
2023-07-19 09:58:54 +0300

    NumberToString

---
+++ NumberToString/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Kata.numberToString(123));
+    }
+}
+class Kata {
+    public static String numberToString(int num) {
+
+        String a = String.valueOf(num);
+
+        return a;
+
+    }
+}
